076275e788f1dafe04f4158c6f6f2c8a769c26c8
Tomwf <chowkirthmas@gmail.com>
2022-04-14 22:48:49 +0200

    test: add tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,131 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator()
+const style = (text) => `<span style='color: green'>${text}</span>`
 
 suite('Unit Tests', () => {
+  suite('toBritish()', () => {
+    test('Mangoes are my favorite fruit.', () => {
+      const text = 'Mangoes are my favorite fruit.'
 
+      assert.strictEqual(translator.toBritish(text), `Mangoes are my ${style('favourite')} fruit.`)
+    })
+
+    test('I ate yogurt for breakfast.', () => {
+      const text = 'I ate yogurt for breakfast.'
+
+      assert.strictEqual(translator.toBritish(text), `I ate ${style('yoghurt')} for breakfast.`)
+    })
+
+    test('We had a party at my friend\'s condo.', () => {
+      const text = 'We had a party at my friend\'s condo.'
+
+      assert.strictEqual(translator.toBritish(text), `We had a party at my friend\'s ${style('flat')}.`)
+    })
+
+    test('Can you toss this in the trashcan for me?', () => {
+      const text = 'Can you toss this in the trashcan for me?'
+
+      assert.strictEqual(translator.toBritish(text), `Can you toss this in the ${style('bin')} for me?`)
+    })
+
+    test('The parking lot was full.', () => {
+      const text = 'The parking lot was full.'
+
+      assert.strictEqual(translator.toBritish(text), `The ${style('car park')} was full.`)
+    })
+
+    test('Like a high tech Rube Goldberg machine.', () => {
+      const text = 'Like a high tech Rube Goldberg machine.'
+
+      assert.strictEqual(translator.toBritish(text), `Like a high tech ${style('Heath Robinson device')}.`)
+    })
+
+    test('To play hooky means to skip class or work.', () => {
+      const text = 'To play hooky means to skip class or work.'
+
+      assert.strictEqual(translator.toBritish(text), `To ${style('bunk off')} means to skip class or work.`)
+    })
+
+    test('No Mr. Bond, I expect you to die.', () => {
+      const text = 'No Mr. Bond, I expect you to die.'
+
+      assert.strictEqual(translator.toBritish(text), `No ${style('Mr')} Bond, I expect you to die.`)
+    })
+
+    test('Dr. Grosh will see you now.', () => {
+      const text = 'Dr. Grosh will see you now.'
+
+      assert.strictEqual(translator.toBritish(text), `${style('Dr')} Grosh will see you now.`)
+    })
+
+    test('Lunch is at 12:15 today.', () => {
+      const text = 'Lunch is at 12:15 today.'
+
+      assert.strictEqual(translator.toBritish(text), `Lunch is at ${style('12.15')} today.`)
+    })
+  })
+
+  suite('toAmerican()', () => {
+    test('We watched the footie match for a while.', () => {
+      const text = 'We watched the footie match for a while.'
+
+      assert.strictEqual(translator.toAmerican(text), `We watched the ${style('soccer')} match for a while.`)
+    })
+
+    test('Paracetamol takes up to an hour to work.', () => {
+      const text = 'Paracetamol takes up to an hour to work.'
+
+      assert.strictEqual(translator.toAmerican(text), `${style('Tylenol')} takes up to an hour to work.`)
+    })
+
+    test('First, caramelise the onions.', () => {
+      const text = 'First, caramelise the onions.'
+
+      assert.strictEqual(translator.toAmerican(text), `First, ${style('caramelize')} the onions.`)
+    })
+
+    test('I spent the bank holiday at the funfair.', () => {
+      const text = 'I spent the bank holiday at the funfair.'
+
+      assert.strictEqual(translator.toAmerican(text), `I spent the ${style('public holiday')} at the ${style('carnival')}.`)
+    })
+
+    test('I had a bicky then went to the chippy.', () => {
+      const text = 'I had a bicky then went to the chippy.'
+
+      assert.strictEqual(translator.toAmerican(text), `I had a ${style('cookie')} then went to the ${style('fish-and-chip shop')}.`)
+    })
+
+    test('I\'ve just got bits and bobs in my bum bag.', () => {
+      const text = 'I\'ve just got bits and bobs in my bum bag.'
+
+      assert.strictEqual(translator.toAmerican(text), `I've just got ${style('odds and ends')} in my ${style('fanny pack')}.`)
+    })
+
+    test('The car boot sale at Boxted Airfield was called off.', () => {
+      const text = 'The car boot sale at Boxted Airfield was called off.'
+
+      assert.strictEqual(translator.toAmerican(text), `The ${style('swap meet')} at Boxted Airfield was called off.`)
+    })
+
+    test('Have you met Mrs Kalyani?', () => {
+      const text = 'Have you met Mrs Kalyani?'
+
+      assert.strictEqual(translator.toAmerican(text), `Have you met ${style('Mrs.')} Kalyani?`)
+    })
+
+    test('Prof Joyner of King\'s College, London.', () => {
+      const text = 'Prof Joyner of King\'s College, London.'
+
+      assert.strictEqual(translator.toAmerican(text), `${style('Prof.')} Joyner of King's College, London.`)
+    })
+
+    test('Tea time is usually around 4 or 4.30.', () => {
+      const text = 'Tea time is usually around 4 or 4.30.'
+
+      assert.strictEqual(translator.toAmerican(text), `Tea time is usually around 4 or ${style('4:30')}.`)
+    })
+  })
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,112 @@ const server = require('../server.js');
 chai.use(chaiHttp);
 
 let Translator = require('../components/translator.js');
+const style = (text) => `<span style='color: green'>${text}</span>`
 
 suite('Functional Tests', () => {
+  test('Translation with text and locale fields', (done) => {
+    const req = {
+      text: 'Mangoes are my favorite fruit.',
+      locale: 'american-to-british'
+    }
 
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, {
+          text:  'Mangoes are my favorite fruit.',
+          translation: `Mangoes are my ${style('favourite')} fruit.`
+        })
+        done()
+      })
+  })
+
+  test('Translation with text and invalid locale field', (done) => {
+    const req = {
+      text: 'I ate yogurt for breakfast.',
+      locale: 'invalid locale'
+    }
+
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, { error: 'Invalid value for locale field' })
+        done()
+      })
+  })
+
+  test('Translation with missing text field', (done) => {
+    const req = {
+      locale: 'american-to-british'
+    }
+
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, { error: 'Required field(s) missing' })
+        done()
+      })
+  })
+
+  test('Translation with missing locale field', (done) => {
+    const req = {
+      text: 'Can you toss this in the trashcan for me?'
+    }
+
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, { error: 'Required field(s) missing' })
+        done()
+      })
+  })
+
+  test('Translation with empty text', (done) => {
+    const req = {
+      text: '',
+      locale: 'american-to-british'
+    }
+
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, { error: 'Translation with empty text' })
+        done()
+      })
+  })
+
+  test('Translation with text that needs no translation', (done) => {
+    const req = {
+      text: 'We watched the footie match for a while.',
+      locale: 'american-to-british'
+    }
+
+    chai.request(server)
+      .post('/api/translate')
+      .send(req)
+      .end((err, res) => {
+        if (err) console.error(err)
+
+        assert.deepEqual(res.body, {
+          text: 'We watched the footie match for a while.',
+          translation: 'Everything looks good to me!'
+        })
+        done()
+      })
+  })
 });
